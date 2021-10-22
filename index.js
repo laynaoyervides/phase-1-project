1672a10e3cce41ba226bc361cabdc1eb19d32edd
Layna Oyervides <laynaoyervides@gmail.com>
2021-10-21 20:05:07 -0600

    Added a db.json file and index.js file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-
+//variables
+//get function for getting images from db.json
+//function for rending images to a car
+function getArtworks () {
+    return fetch("localhost:3000/artworks")
+    .then(res => res.json())
+}
+//function for rendering each artwork card
+getArtworks.then () artworks => {
+    artworks.forEach(piece => {
+        renderCard(piece);
+    });
+}
+//function to make each card
+function renderCard(piece) {
+    
+}
